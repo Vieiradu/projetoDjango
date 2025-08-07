276038ea9775e169f432874786c9b5be72bc2c18
Luiz Vieira <duullivee@gmail.com>
2025-08-06 21:45:29 -0300

    feat: add home templates page and update views for recipes app

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the home page!")
+    return render(request, 'recipes/home.html')
 
-def contact(request):
-    return HttpResponse("This is the contact page.")
-
-def about(request):
-    return HttpResponse("This is the about page.")
